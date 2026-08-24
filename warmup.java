c3508e7d35f7d9de0e9f12b1257e6d55df73e413
marian-stanley <mariringstan@gmail.com>
2026-08-24 16:41:56 +0000

    new changes and it's problem 1 and 2

--- warmup.java
+++ warmup.java
@@ -5,9 +5,15 @@ public class warmup {
             total += i;
         }
         System.out.println(total / 2);
-    }
-    //Predicted: Out 15 (forgot to divide in class) 
+    
+//Predicted: Out 15 (forgot to divide in class) 
 //Output: 7
 //Why: code adds the numbers 1-5 together then divides by 2, it being an int needs a whole number so 14.5 is rounded down to 7.
-}
+
+    int [] a = {3,7,2,9}; 
+    for (int i = 0; i <= a.length; i++) {
+        System.out.println(a[i]);
+    }
+
+}}
 
