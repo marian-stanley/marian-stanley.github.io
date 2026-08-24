8ee9f8397c7187e66051e07af787bf910f68e6d1
Marian Stanley <marian.stanley2006@gmail.com>
2026-08-24 11:14:12 -0600

    new version

--- warmup2.java
+++
@@ -1,8 +0,0 @@
-public class warmup2 {
-     public static void arrayTest() {
-        int [] a = {3,7,2,9};
-        for (int i = 0; i <= a.length; i++) {
-            System.out.println(a[i]);
-        }
-    }
-}
