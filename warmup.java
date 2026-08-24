d3bd1b689044ce6cc9f801590047d3c1ec662a09
marian-stanley <mariringstan@gmail.com>
2026-08-24 10:08:39 -0600

    Add files via upload

---
+++ warmup.java
@@ -0,0 +1,17 @@
+public class warmup {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 5; i++) {
+            total += i;
+        }
+        System.out.println(total / 2);
+    }
+
+    public static void arrayTest() {
+        int [] a = {3,7,2,9};
+        for (int i = 0; i <= a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+
