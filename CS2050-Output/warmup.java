666dacfe1d21b4bc46da21a7f4f65f2e7dba2ff5
Marian Stanley <marian.stanley2006@gmail.com>
2026-08-25 18:39:52 -0600

    final file changes - code projects can be found in CS2050 output now

---
+++ CS2050-Output/warmup.java
@@ -0,0 +1,39 @@
+public class warmup {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 5; i++) {
+            total += i;
+        }
+        System.out.println(total / 2);
+    
+//Predicted: Out 15 (forgot to divide in class) 
+//Output: 7
+//Why: code adds the numbers 1-5 together then divides by 2, it being an int needs a whole number so 14.5 is rounded down to 7.
+
+    int [] a = {3,7,2,9}; 
+    for (int i = 0; i <= a.length; i++) {
+        System.out.println(a[i]);
+    }
+
+
+//Perdicted: 3,7,2,9
+//Output: 3,7,2,9
+//Why: Code goes through the array and prints each value but is asking for an addtional index 4 that doesn't exist. 
+
+    String s1 = "cs2050";
+    String s2 = new String ("cs2050");
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+    System.out.println(s1);
+
+//Predicted: false, true
+//Output: nothing
+//Why: s1 is a direct assignment and s2 is a new object, but im actually not sure why it's not printing
+
+
+    }
+}
+
+
+
+
